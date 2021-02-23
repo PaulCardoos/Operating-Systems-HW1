a9a3a34c77a7cb7b9e2903d78c384164e12c579a
lukemvu <luke.m.vu@gmail.com>
2021-02-23 13:15:53 -0500

    added queue import and removed circular buffer

--- tty.h
+++ tty.h
@@ -5,23 +5,21 @@
 *
 *       private header file for tty drivers
 *       apps should not include this header
+*
+*       2/23/2021
+*       imported queue
+*       removed uncessary circular buffer variables
+*
+*
 */
 
 #ifndef TTY_H
 #define TTY_H
-
+#include "queue/queue.h"
 #define MAXBUF 6
 
 struct tty {
   int echoflag;			/* echo chars in read */
-  char rbuf[MAXBUF];            /* typeahead chars */
-  int rin;                    /* index into receive buffer */
-  int rout;                   /* index out of receive buffer */
-  int rnum;                   /* number of characters in rcvr buffer */
-  char tbuf[MAXBUF];            /* output chars (for transmit) */
-  int tin;                    /* index into transmit buffer */
-  int tout;                   /* index out of transmit buffer */
-  int tnum;                   /* number of characters in xmit buffer */
 };
 
 extern struct tty ttytab[];
@@ -32,4 +30,4 @@ int ttyread(int dev, char *buf, int nchar);
 int ttywrite(int dev, char *buf, int nchar);
 int ttycontrol(int dev, int fncode, int val);
 
-#endif 
+#endif
